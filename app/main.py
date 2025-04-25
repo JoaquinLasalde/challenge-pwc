6839b0c658cacb80a0ef746625a7055e503861ed
JoaquinLasalde <joaquinlasalde@gmail.com>
2025-04-25 12:22:06 -0300

    Update port in main.py to match run.py (8001)

--- app/main.py
+++ app/main.py
@@ -49,4 +49,4 @@ def on_startup():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True) 
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=True) 
